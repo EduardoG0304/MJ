4f1974ff7d28981eed3c09c2182a58f3e88fb55f
EduardoG0304 <a22310546@ceti.mx>
2025-07-28 10:29:26 -0600

    document

--- src/app/_document.js
+++ src/app/_document.js
@@ -1,6 +1,16 @@
-<body>
-  <div id="root">
-    <Main />
-    <NextScript />
-  </div>
-</body>
+// pages/_document.js
+import { Html, Head, Main, NextScript } from 'next/document';
+
+export default function Document() {
+  return (
+    <Html lang="es">
+      <Head />
+      <body>
+        <div id="root"> {/* Contenedor raíz */}
+          <Main /> {/* Aquí se renderiza la aplicación */}
+        </div>
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
