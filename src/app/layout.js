ca3ea76d211cca597d56d4bd0d11f1a6be5a7af5
EduardoG0304 <a22310546@ceti.mx>
2025-07-28 12:17:10 -0600

    icon

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,12 +1,22 @@
 import { Inter } from 'next/font/google';
 import './globals.css';
-import ModalProvider from './components/modal-provider'; // Ajusta la ruta según tu estructura
+import ModalProvider from './components/modal-provider';
 
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
   title: 'MJ SPORT PHOTOGRAPHY - Fotografías de Eventos Deportivos',
   description: 'Encuentra las mejores fotografías de tus eventos deportivos. Búsqueda por número o rostro.',
+  icons: {
+    icon: '/images/logo_mj.ico', // Asegúrate de que el archivo esté en la carpeta public
+    // También puedes agregar otros tamaños si los tienes:
+    // shortcut: '/shortcut-icon.png',
+    // apple: '/apple-icon.png',
+    // other: {
+    //   rel: 'apple-touch-icon-precomposed',
+    //   url: '/apple-touch-icon-precomposed.png',
+    // },
+  },
 };
 
 export default function RootLayout({ children }) {
@@ -14,7 +24,7 @@ export default function RootLayout({ children }) {
     <html lang="es">
       <body className={`${inter.className} bg-white text-gray-900`}>
         <div id="root" className="min-h-screen flex flex-col">
-          <ModalProvider /> {/* 👈 Configura react-modal */}
+          <ModalProvider />
           {children}
         </div>
       </body>
