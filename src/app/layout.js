de0cc1d86f805107e30ed2447a3ea2a83eefd6a3
EduardoG0304 <a22310546@ceti.mx>
2025-07-28 12:31:24 -0600

    photos

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,10 +5,26 @@ import ModalProvider from './components/modal-provider';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
-  title: 'MJ SPORT PHOTOGRAPHY - Fotografías de Eventos Deportivos',
-  description: 'Encuentra las mejores fotografías de tus eventos deportivos. Búsqueda por número o rostro.',
+  title: 'MJ SPORT PHOTOGRAPHY',
+  description: '...',
   icons: {
-    icon: '/images/mj_logo.ico', // Ruta corregida
+    icon: [
+      { url: '/images/mj_logo.ico' }, // Tradicional (ICO)
+      { url: '/images/icon-16x16.png', sizes: '16x16', type: 'image/png' },
+      { url: '/images/icon-32x32.png', sizes: '32x32', type: 'image/png' },
+      { url: '/images/icon-96x96.png', sizes: '96x96', type: 'image/png' },
+    ],
+    apple: [
+      { url: '/icon-192x192.png', sizes: '192x192', type: 'image/png' }, // Para iOS
+    ],
+    other: [
+      {
+        rel: 'apple-touch-icon-precomposed',
+        url: '/icon-512x512.png',
+        sizes: '512x512',
+        type: 'image/png',
+      },
+    ],
   },
 };
 
