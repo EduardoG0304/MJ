a6ad39b444bb41c9fd02e512abcb8440265850ef
EduardoG0304 <a22310546@ceti.mx>
2025-07-28 10:34:38 -0600

    route

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,6 @@
 import { Inter } from 'next/font/google';
 import './globals.css';
-import ModalProvider from '@/components/modal-provider'; // Ajusta la ruta según tu estructura
+import ModalProvider from './components/modal-provider'; // Ajusta la ruta según tu estructura
 
 const inter = Inter({ subsets: ['latin'] });
 
